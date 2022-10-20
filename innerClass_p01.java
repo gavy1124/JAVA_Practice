662ea09277fe18bd3443892a08df9cc504b9b8a2
KIM-TAEWON <gavy1124@naver.com>
2022-10-21 00:28:22 +0900

    7-49 내부클래스 제어자

---
+++ innerClass_p01.java
@@ -0,0 +1,26 @@
+package JAVA_Practice;
+
+import JAVA_Practice.Outer2.StaticInner;
+
+class Outer2{
+	class Inner2{
+		int iv = 100;
+	}
+	static class StaticInner{
+		int iv = 200;
+		static int cv = 300;
+	}
+
+}
+public class innerClass_p01 {
+	public static void main(String[] args) {
+		Outer2 oc = new Outer2(); // 클래스 내부 Instance 클래스 사용시 
+		Outer2.Inner2 in = oc.new Inner2();
+		System.out.println(in.iv);
+		// --------------------------------------------------
+		
+		
+		Outer2.StaticInner st = new Outer2.StaticInner(); // 클래스내부 statc클래스 사용시
+		System.out.println(st.iv);
+	}
+}
