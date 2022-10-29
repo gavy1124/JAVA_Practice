dd037e0a8e7878954dec7e539b444d0e78000d89
KIM-TAEWON <gavy1124@naver.com>
2022-10-29 14:42:30 +0900

    다형성연습2

--- TestShape.java
+++ TestShape.java
@@ -1,4 +1,4 @@
-package oop.polymorphism;
+package Git;
 
 
 public class TestShape {
@@ -12,4 +12,65 @@ public class TestShape {
 			shape[ i ].print();
 		}
 	}
+}
+
+
+
+abstract class Shape{
+	//매개변수
+	protected double area;
+	private String name;
+	//생성자
+	public void Shape() {
+		
+	}
+	
+	public Shape(String name) {
+		super();
+		this.name = name;
+	}
+
+	public Shape(double area, String name) {
+		super();
+		this.area = area;
+		this.name = name;
+	}
+	
+	//메서드
+	abstract public void calculationArea();
+	public void print() {
+		System.out.println(name + "의 면적은 " + area);
+	}
+}
+
+class Circle extends Shape{
+	//매개변수
+	private double radius;
+	//생성자
+
+	public Circle(String name, double radius) {
+		super(name);
+		this.radius = radius;
+	}
+	public void calculationArea() {
+		area = radius*radius * Math.PI;
+	}
+	
+	
+}
+
+class Rectangular extends Shape{
+	//매개변수
+	private double width;
+	private double height;
+	//생성자
+	public Rectangular(String name, double width, double height) {
+		super(name);
+		this.width = width;
+		this.height = height;
+	}
+	public void calculationArea() {
+		area = width * height;
+	}
+	
 }
